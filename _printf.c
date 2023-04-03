068424a8688cc99ef454c052be5ac62c6f11186d
Jordan DIGHO <dighodonmezatankeujordan@gmail.com>
2023-04-03 13:19:29 +0100

    Commitingù

--- _printf.c
+++ _printf.c
@@ -43,21 +43,3 @@ i++;
 }
 return (size);
 }
-
-
-
-int main(void)
-{
-	int len, len2;
-
-	len = _printf("Let's print a simple sentence.\n");
-	len2 = printf("Let's print a simple sentence.\n");
-	fflush(stdout);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
-	return (0);
-}
